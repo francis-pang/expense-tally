d07fa6b769ccfecf632bb8f725d3dfe3912441e7
Francis Pang <boyshawn@hotmail.com>
2018-11-10 01:55:28 +0800

    Port the logic of parsing the CSV into a standalone parser

--- src/main/java/expense_tally/Application.java
+++ src/main/java/expense_tally/Application.java
@@ -1,6 +1,7 @@
 package expense_tally;
 
 import expense_tally.model.TransactionCsv;
+import expense_tally.service.CsvParser;
 
 import java.io.*;
 import java.text.DateFormat;
@@ -19,51 +20,9 @@ public class Application {
         System.out.println(file.getAbsolutePath());
 
         List<TransactionCsv> transactionCsvList = new ArrayList<>();
-
+        CsvParser transactionCsvParser = new CsvParser();
         try {
-            // Ignore until start with Transaction Date
-            BufferedReader csvBufferedReader = new BufferedReader(new FileReader(filename));
-            String line;
-            do {
-                line = csvBufferedReader.readLine();
-
-            }while (!line.startsWith("Transaction Date"));
-
-            //09 Nov 2018
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
-
-            // Skip empty lines, then use getter to retrieve information
-            line = csvBufferedReader.readLine();
-            int counter = 0;
-            while (line != null){
-                if (line.length() == 0) {
-                    line = csvBufferedReader.readLine();
-                    continue;
-                }
-                String[] csvElements = line.split(",");
-                TransactionCsv transactionCsv = new TransactionCsv();
-                transactionCsv.setTransactionDate(LocalDate.parse(csvElements[0], formatter));
-                transactionCsv.setReference(csvElements[1]);
-                transactionCsv.setDebitAmount((csvElements[2].isBlank()) ? 0.00 : Double.parseDouble(csvElements[2]));
-                transactionCsv.setCreditAmount((csvElements[3].isBlank()) ? 0.00 : Double.parseDouble(csvElements[3]));
-                if (csvElements.length >= 5) {
-                    transactionCsv.setTransactionRef1(csvElements[4]);
-                } else {
-                    transactionCsv.setTransactionRef1("");
-                }
-                if (csvElements.length >= 6) {
-                    transactionCsv.setTransactionRef2(csvElements[5]);
-                } else {
-                    transactionCsv.setTransactionRef2("");
-                }
-                if (csvElements.length >= 7) {
-                    transactionCsv.setTransactionRef3(csvElements[6]);
-                } else {
-                    transactionCsv.setTransactionRef3("");
-                }
-                transactionCsvList.add(transactionCsv);
-                line = csvBufferedReader.readLine();
-            }
+            transactionCsvList = transactionCsvParser.parseCsvFile(filename);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
