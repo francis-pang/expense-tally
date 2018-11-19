d82bb048a9e5a5424bf0d5efd59e711776ece207
Francis Pang <francis.pang@sgdigital.com>
2018-11-19 15:24:04 +0800

    Break up the main code flow into individual static methods

--- src/main/java/expense_tally/Application.java
+++ src/main/java/expense_tally/Application.java
@@ -25,77 +25,83 @@ import java.lang.Long;
 public class Application {
     private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
 
-    public static void main (String args[]) {
-        // assumes the current class is called MyLogger
-
-
-        // Read CSV file
-        final String filename = "src/main/resource/csv/3db7c598cadc80893570d55a0243df1c.P000000013229282.csv";
-        File file = new File(filename);
+    private static List<CsvTransaction> readCsv(String csvFilePath) {
+        File file = new File(csvFilePath);
         System.out.println(file.getAbsolutePath());
 
         List<CsvTransaction> csvTransactions = new ArrayList<>();
         CsvParser transactionCsvParser = new CsvParser();
         try {
-            csvTransactions = transactionCsvParser.parseCsvFile(filename);
+            csvTransactions = transactionCsvParser.parseCsvFile(csvFilePath);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return csvTransactions;
+    }
 
-        // Display
-        /*
-        for (CsvTransaction csvLine : csvTransactions) {
-            System.out.println(csvLine.toString());
-        }
-        */
-
+    private static List<ExpenseReport> importDataFromDatabase(Connection databaseConnection) throws SQLException {
         // Connect to expense_tally.database
         List<ExpenseReport> expenseReports = new ArrayList<>();
+        // Read all records into objects
+        Statement retrieveAlLStatement = databaseConnection.createStatement();
+        ResultSet retrieveAllResultSet = retrieveAlLStatement.executeQuery("SELECT * FROM expense_report");
+        System.out.println(retrieveAllResultSet.getFetchSize());
+        while(retrieveAllResultSet.next()) {
+            ExpenseReport expenseReport = new ExpenseReport();
+            expenseReport.setId(retrieveAllResultSet.getInt("_id"));
+            expenseReport.setAccount(retrieveAllResultSet.getString("account"));
+            expenseReport.setAmount(retrieveAllResultSet.getString("amount"));
+            expenseReport.setCategory(retrieveAllResultSet.getString("category"));
+            expenseReport.setSubcategory(retrieveAllResultSet.getString("subcategory"));
+            expenseReport.setPaymentMethod(retrieveAllResultSet.getString("payment_method"));
+            expenseReport.setDescription(retrieveAllResultSet.getString("description"));
+            expenseReport.setExpensed(retrieveAllResultSet.getLong("expensed"));
+            expenseReport.setModified(retrieveAllResultSet.getLong("modified"));
+            expenseReport.setReferenceNumber(retrieveAllResultSet.getString("reference_number"));
+            expenseReport.setStatus(retrieveAllResultSet.getString("status"));
+            expenseReport.setProperty(retrieveAllResultSet.getString("property"));
+            expenseReport.setProperty2(retrieveAllResultSet.getString("property2"));
+            expenseReport.setProperty3(retrieveAllResultSet.getString("property3"));
+            expenseReport.setProperty3(retrieveAllResultSet.getString("property4"));
+            expenseReport.setProperty5(retrieveAllResultSet.getString("property5"));
+            expenseReport.setTax(retrieveAllResultSet.getString("tax"));
+            expenseReport.setExpenseTag(retrieveAllResultSet.getString("expense_tag"));
+            expenseReports.add(expenseReport);
+        }
+
+        retrieveAllResultSet.close();
+        databaseConnection.close();
+        return expenseReports;
+    }
+
+    public static void main (String args[]) {
+        // assumes the current class is called MyLogger
+
+        // Read CSV file
+        final String filename = "src/main/resource/csv/3db7c598cadc80893570d55a0243df1c.P000000013229282.csv";
+
+        List<CsvTransaction> csvTransactions = readCsv(filename);
+
+        List<ExpenseReport> expenseReports = null;
         try {
             Connection databaseConnection = SqlLiteConnectionManager.connect();
-
-            System.out.println("Network established");
-
-            // Read all records into objects
-            Statement retrieveAlLStatement = databaseConnection.createStatement();
-            ResultSet retrieveAllResultSet = retrieveAlLStatement.executeQuery("SELECT * FROM expense_report");
-            System.out.println(retrieveAllResultSet.getFetchSize());
-            while(retrieveAllResultSet.next()) {
-                ExpenseReport expenseReport = new ExpenseReport();
-                expenseReport.setId(retrieveAllResultSet.getInt("_id"));
-                expenseReport.setAccount(retrieveAllResultSet.getString("account"));
-                expenseReport.setAmount(retrieveAllResultSet.getString("amount"));
-                expenseReport.setCategory(retrieveAllResultSet.getString("category"));
-                expenseReport.setSubcategory(retrieveAllResultSet.getString("subcategory"));
-                expenseReport.setPaymentMethod(retrieveAllResultSet.getString("payment_method"));
-                expenseReport.setDescription(retrieveAllResultSet.getString("description"));
-                expenseReport.setExpensed(retrieveAllResultSet.getLong("expensed"));
-                expenseReport.setModified(retrieveAllResultSet.getLong("modified"));
-                expenseReport.setReferenceNumber(retrieveAllResultSet.getString("reference_number"));
-                expenseReport.setStatus(retrieveAllResultSet.getString("status"));
-                expenseReport.setProperty(retrieveAllResultSet.getString("property"));
-                expenseReport.setProperty2(retrieveAllResultSet.getString("property2"));
-                expenseReport.setProperty3(retrieveAllResultSet.getString("property3"));
-                expenseReport.setProperty3(retrieveAllResultSet.getString("property4"));
-                expenseReport.setProperty5(retrieveAllResultSet.getString("property5"));
-                expenseReport.setTax(retrieveAllResultSet.getString("tax"));
-                expenseReport.setExpenseTag(retrieveAllResultSet.getString("expense_tag"));
-                expenseReports.add(expenseReport);
-            }
-
-            retrieveAllResultSet.close();
-            databaseConnection.close();
-
+            expenseReports = importDataFromDatabase(databaseConnection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Network established");
 
         // Data mapping
         Map<Double, List<ExpenseTransaction>> expenseTransactionMap = ExpenseTransactionMapper.mapExpenseReports(expenseReports);
 
         // Reconcile data
+        reconcileBankData(csvTransactions, expenseTransactionMap);
+    }
+
+    private static void reconcileBankData (List<CsvTransaction> csvTransactions, Map<Double, List<ExpenseTransaction>> expenseTransactionMap) {
         int numberOfNoMatchTransaction = 0;
         for (CsvTransaction csvTransaction : csvTransactions) {
             if (csvTransaction.getDebitAmount() == 0) {
@@ -110,10 +116,10 @@ public class Application {
             }
             int noOfMatchingTransaction = 0;
             for(ExpenseTransaction matchingExpenseTransaction : expenseTransactionList) {
-                LOGGER.fine("Comparing " + csvTransaction.getTransactionDate() + " vs " + LocalDate.ofInstant(matchingExpenseTransaction.getExpensedTime(), ZoneId.of("UTC").normalized()));
+                //LOGGER.fine("Comparing " + csvTransaction.getTransactionDate() + " vs " + LocalDate.ofInstant(matchingExpenseTransaction.getExpensedTime(), ZoneId.of("UTC").normalized()));
                 Duration transactionTimeDifference = Duration.between(matchingExpenseTransaction.getExpensedTime(), csvTransaction.getTransactionDate().atStartOfDay().toInstant(ZoneOffset.UTC));
                 if(transactionTimeDifference.toHours() > -24 &&
-                   transactionTimeDifference.toHours() <= 48) {
+                        transactionTimeDifference.toHours() <= 48) {
                     noOfMatchingTransaction++;
                 }
             }
