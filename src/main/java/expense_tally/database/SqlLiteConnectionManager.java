bf38459e90d5bce427e3e5bfe8fcfa3c2b2bc19f
Francis Pang <francis.pang@sgdigital.com>
2018-11-22 13:44:31 +0800

    Take out any negative time difference between expense transaction from file and database. And move hardcoded number to constant variables

--- src/main/java/expense_tally/database/SqlLiteConnectionManager.java
+++ src/main/java/expense_tally/database/SqlLiteConnectionManager.java
@@ -5,8 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqlLiteConnectionManager {
-    public static Connection connect() throws SQLException {
-        final String databaseFile = "jdbc:sqlite:D:/code/expense-tally/src/main/resource/database/2018-11-09.db";
+    private String databaseFile;
+
+    public SqlLiteConnectionManager(String databaseFile) {
+        this.databaseFile = databaseFile;
+    }
+
+    public Connection connect() throws SQLException {
         Connection databaseConnection = DriverManager.getConnection(databaseFile);
         return databaseConnection;
     }
