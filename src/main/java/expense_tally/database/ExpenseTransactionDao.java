30235f37fd9e7ccf27d690a3a65049378c524676
Francis Pang <boyshawn@hotmail.com>
2018-11-29 09:30:48 +0800

    ExpenseTransactionDAO and ExpenseReconciler This is restructure of the code to adopt 2 design patterns: 1. ExpenseTransactionDAO ExpenseTransactionDAO adopts the Data Access Object pattern to create an additional layer of abstraction to the retrieval of Expense Transaction objects from the sqLite database. With this layer, the database query will be encapsulated, together with the existing ExpenseTransactionMapper. 2. ExpenseReconciler ExpenseReconciler adopts the Service abstraction pattern. It create an layer of abstraction of how the data retrieved from the CSV and the database are used for reconcilation. It allows for expansion in the future without affects the main application code.
    A first attempt to adopt the Data Access Object pattern and Service pattern
    So now the retrieval of the expense transaction is being encapsulated in the data object access class. So the knowledge of access the database is hidden from the application class. A new service class ExpenseReconciler is create to perform a business service of reconciling the given data from the CSV file and the database

---
+++ src/main/java/expense_tally/database/ExpenseTransactionDao.java
@@ -0,0 +1,67 @@
+package expense_tally.database;
+
+import expense_tally.model.ExpenseTransaction;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Data access object for expense transaction
+ */
+public class ExpenseTransactionDao {
+    private SqlLiteConnectionManager sqlLiteConnectionManager;
+
+    public ExpenseTransactionDao(String databaseFile) {
+        SqlLiteConnectionManager sqlLiteConnectionManager = new SqlLiteConnectionManager(databaseFile);
+    }
+
+    public Map<Double, List<ExpenseTransaction>> getAllExpenseTransactions() throws SQLException {
+        Map<Double, List<ExpenseTransaction>> expenseTransactionMap = new HashMap();
+
+        Connection databaseConnection = sqlLiteConnectionManager.connect();
+        List<ExpenseReport> expenseReports = importDataFromDatabase(databaseConnection);
+        return ExpenseTransactionMapper.mapExpenseReports(expenseReports);
+    }
+
+    private List<ExpenseReport> importDataFromDatabase(Connection databaseConnection) throws SQLException {
+        // Connect to expense_tally.database
+        List<ExpenseReport> expenseReports = new ArrayList<>();
+        // Read all records into objects
+        Statement retrieveAlLStatement = databaseConnection.createStatement();
+        ResultSet retrieveAllResultSet = retrieveAlLStatement.executeQuery("SELECT * FROM expense_report");
+        System.out.println(retrieveAllResultSet.getFetchSize());
+        while(retrieveAllResultSet.next()) {
+            ExpenseReport expenseReport = new ExpenseReport();
+            expenseReport.setId(retrieveAllResultSet.getInt("_id"));
+            expenseReport.setAccount(retrieveAllResultSet.getString("account"));
+            expenseReport.setAmount(retrieveAllResultSet.getString("amount"));
+            expenseReport.setCategory(retrieveAllResultSet.getString("category"));
+            expenseReport.setSubcategory(retrieveAllResultSet.getString("subcategory"));
+            expenseReport.setPaymentMethod(retrieveAllResultSet.getString("payment_method"));
+            expenseReport.setDescription(retrieveAllResultSet.getString("description"));
+            expenseReport.setExpensed(retrieveAllResultSet.getLong("expensed"));
+            expenseReport.setModified(retrieveAllResultSet.getLong("modified"));
+            expenseReport.setReferenceNumber(retrieveAllResultSet.getString("reference_number"));
+            expenseReport.setStatus(retrieveAllResultSet.getString("status"));
+            expenseReport.setProperty(retrieveAllResultSet.getString("property"));
+            expenseReport.setProperty2(retrieveAllResultSet.getString("property2"));
+            expenseReport.setProperty3(retrieveAllResultSet.getString("property3"));
+            expenseReport.setProperty3(retrieveAllResultSet.getString("property4"));
+            expenseReport.setProperty5(retrieveAllResultSet.getString("property5"));
+            expenseReport.setTax(retrieveAllResultSet.getString("tax"));
+            expenseReport.setExpenseTag(retrieveAllResultSet.getString("expense_tag"));
+            expenseReports.add(expenseReport);
+        }
+
+        retrieveAllResultSet.close();
+        databaseConnection.close();
+        return expenseReports;
+    }
+}
