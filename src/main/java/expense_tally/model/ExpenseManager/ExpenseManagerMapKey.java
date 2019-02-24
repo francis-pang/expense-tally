13ba2ffc30fe1fb28f62b382273ea58e8f2d02c3
boyshawn <boyshawn@hotmail.com>
2019-02-24 18:25:09 +0800

    Add equals, hashcode and toString method to ExpenseManagerMapKey, and small refactoring to ExpenseReconciler

--- src/main/java/expense_tally/model/ExpenseManager/ExpenseManagerMapKey.java
+++ src/main/java/expense_tally/model/ExpenseManager/ExpenseManagerMapKey.java
@@ -1,5 +1,8 @@
 package expense_tally.model.ExpenseManager;
 
+import java.util.Objects;
+import java.util.StringJoiner;
+
 public class ExpenseManagerMapKey {
     private PaymentMethod paymentMethod;
     private Double amount;
@@ -12,15 +15,33 @@ public class ExpenseManagerMapKey {
         return paymentMethod;
     }
 
-    public void setPaymentMethod(PaymentMethod paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
     public Double getAmount() {
         return amount;
     }
 
     public void setAmount(Double amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExpenseManagerMapKey)) return false;
+        ExpenseManagerMapKey that = (ExpenseManagerMapKey) o;
+        return paymentMethod == that.paymentMethod &&
+                Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(paymentMethod, amount);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ExpenseManagerMapKey.class.getSimpleName() + "[", "]")
+                .add("paymentMethod=" + paymentMethod)
+                .add("amount=" + amount)
+                .toString();
     }
 }
