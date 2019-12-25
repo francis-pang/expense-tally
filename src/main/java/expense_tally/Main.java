7d97d99649824f1426bc359d3242adfba1a7a554
Francis Pang <boyshawn@hotmail.com>
2019-12-25 10:30:07 +0800

    Create the base user interface runnable from IDE

--- src/main/java/expense_tally/Main.java
+++ src/main/java/expense_tally/Main.java
@@ -1,35 +1,49 @@
 package expense_tally;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.sql.SQLException;
+import java.net.URL;
 
 /**
  * This class acts as the Dependency Injection container. It create all the dependencies and inject into the rest of
  * the class
  */
-public class Main {
+public class Main extends Application {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        final int INSUFFICIENT_PARAMETERS_ERR_CODE = 1;
-        final int CSV_FILE_PARSING_ERR_CODE = 2;
-        final int DATABASE_ERR_CODE = 3;
+//    public static void main(String[] args) {
+//        final int INSUFFICIENT_PARAMETERS_ERR_CODE = 1;
+//        final int CSV_FILE_PARSING_ERR_CODE = 2;
+//        final int DATABASE_ERR_CODE = 3;
+//
+//        //TODO: For now, we ignore any parameters after 2nd parameters, next time we can handle them.
+//        try {
+//            ExpenseAccountant expenseAccountant = new ExpenseAccountant(args);
+//            expenseAccountant.reconcileData();
+//        } catch (IOException ioException) {
+//            LOGGER.error("Error reading CSV file", ioException);
+//            System.exit(CSV_FILE_PARSING_ERR_CODE);
+//            //TODO: Print a error message, then exit the program
+//        } catch (SQLException sqlException) {
+//            LOGGER.error("Error reading from database", sqlException);
+//            //TODO: Print a error message, then exit the program
+//            System.exit(DATABASE_ERR_CODE);
+//        }
+//    }
 
-        //TODO: For now, we ignore any parameters after 2nd parameters, next time we can handle them.
-        try {
-            ExpenseAccountant expenseAccountant = new ExpenseAccountant(args);
-            expenseAccountant.reconcileData();
-        } catch (IOException ioException) {
-            LOGGER.error("Error reading CSV file", ioException);
-            System.exit(CSV_FILE_PARSING_ERR_CODE);
-            //TODO: Print a error message, then exit the program
-        } catch (SQLException sqlException) {
-            LOGGER.error("Error reading from database", sqlException);
-            //TODO: Print a error message, then exit the program
-            System.exit(DATABASE_ERR_CODE);
-        }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/ui/ui.fxml");
+        loader.setLocation(xmlUrl);
+        Scene root = loader.load();
+
+        primaryStage.setScene(root);
+        primaryStage.show();
     }
 }
