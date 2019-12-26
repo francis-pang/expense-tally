9c1ec38b7db6a0267aea37310a395183ac9cc258
Francis Pang <boyshawn@hotmail.com>
2019-12-27 03:58:31 +0800

    Refactor to adopt typesafe mocking statement in SqlLiteConnectionTest, reducing unit test execution time.

--- src/test/java/expense_tally/expense_manager/SqlLiteConnectionTest.java
+++ src/test/java/expense_tally/expense_manager/SqlLiteConnectionTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Connection;
@@ -11,21 +12,21 @@ import java.sql.SQLException;
 
 @ExtendWith(MockitoExtension.class)
 class SqlLiteConnectionTest {
-    private DatabaseConnectable databaseConnectable = new SqlLiteConnection("test.db");
-    private DatabaseConnectable spyDatabaseConnectable = Mockito.spy(databaseConnectable);
+    @Spy
+    private DatabaseConnectable spyDatabaseConnectable;
 
     @Test
     void connect_connectionSuccess() throws SQLException {
         Connection mockConnection = Mockito.mock(Connection.class);
-        Mockito.doReturn(mockConnection).when(spyDatabaseConnectable).connect();
-        Assertions.assertThat(spyDatabaseConnectable.connect()).isEqualTo(mockConnection);
+        Mockito.when(spyDatabaseConnectable.connect()).thenReturn(mockConnection);
+        Assertions.assertThat(spyDatabaseConnectable.connect())
+            .isEqualTo(mockConnection);
     }
 
     @Test
     void connect_connectionFail() throws SQLException {
-        Mockito.doThrow(SQLException.class).when(spyDatabaseConnectable).connect();
-        Assertions.assertThatThrownBy(() -> {
-            spyDatabaseConnectable.connect();
-        }).isInstanceOf(SQLException.class);
+        Mockito.when(spyDatabaseConnectable.connect()).thenThrow(SQLException.class);
+        Assertions.assertThatThrownBy(() -> spyDatabaseConnectable.connect())
+            .isInstanceOf(SQLException.class);
     }
 }
