824f9cd7a13b351835705013c48c1473be1c197a
Francis Pang <boyshawn@hotmail.com>
2020-05-31 04:15:23 +0800

    Combine null and class check in equals

--- src/main/java/expense_tally/expense_manager/persistence/ExpenseReport.java
+++ src/main/java/expense_tally/expense_manager/persistence/ExpenseReport.java
@@ -137,10 +137,7 @@ public final class ExpenseReport {
     if (this == o) {
       return true;
     }
-    if (o == null) {
-      return false;
-    }
-    if (getClass() != o.getClass()) {
+    if (o == null || getClass() != o.getClass()) {
       return false;
     }
     ExpenseReport that = (ExpenseReport) o;
