f12235047a948c74e562d8994072fcc25651d7b7
boyshawn <boyshawn@hotmail.com>
2020-09-12 23:36:33 +0800

    POM Split
    Split the project several smaller modules so that easier to package, and smaller in size

---
+++ expense-manager/src/test/java/expense_tally/expense_manager/transformation/ExpenseSubCategoryTest.java
@@ -0,0 +1,40 @@
+package expense_tally.expense_manager.transformation;
+
+import expense_tally.model.persistence.transformation.ExpenseSubCategory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExpenseSubCategoryTest {
+
+  @Test
+  void resolve_sports() {
+    assertThat(ExpenseSubCategory.resolve(ExpenseSubCategory.SPORTS.value()))
+        .isNotNull()
+        .isEqualTo(ExpenseSubCategory.SPORTS);
+  }
+
+  @Test
+  void resolve_notFound() {
+    assertThat(ExpenseSubCategory.resolve("Not Exists"))
+        .isNull();
+  }
+
+  @Test
+  void resolve_null() {
+    assertThat(ExpenseSubCategory.resolve(null))
+        .isNull();
+  }
+
+  @Test
+  void resolve_emptyString() {
+    assertThat(ExpenseSubCategory.resolve(""))
+        .isNull();
+  }
+
+  @Test
+  void value() {
+    assertThat(ExpenseSubCategory.ALCOHOL_AND_RESTAURANT.value())
+        .isEqualTo("Alcohol/ Restaurant");
+  }
+}
--- src/main/java/expense_tally/csv_parser/InvalidReferenceDateException.java
+++
@@ -1,9 +0,0 @@
-package expense_tally.csv_parser;
-
-public final class InvalidReferenceDateException extends Exception {
-  private static final long serialVersionUID = 10763608L;
-
-  public InvalidReferenceDateException(String message) {
-    super(message);
-  }
-}
