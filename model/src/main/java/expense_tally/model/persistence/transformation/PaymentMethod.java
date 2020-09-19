026a573a7b3ceaeb6a78939a8c0dcae752cabf1d
boyshawn <boyshawn@hotmail.com>
2020-09-20 00:48:47 +0800

    Payment Method
    Add a new payment for YouTrip card. Rename existing debit card, since not unique anymore

--- model/src/main/java/expense_tally/model/persistence/transformation/PaymentMethod.java
+++ model/src/main/java/expense_tally/model/persistence/transformation/PaymentMethod.java
@@ -6,7 +6,8 @@ package expense_tally.model.persistence.transformation;
 public enum PaymentMethod {
   CASH("Cash"),
   CREDIT_CARD("Credit Card"),
-  DEBIT_CARD("Debit 5548-2741-0014-1067"),
+  DBS_DEBIT_CARD("Debit 5548-2741-0014-1067"),
+  YOUTRIP_DEBIT_CARD("Debit 5162-4000-1897-6709"),
   ELECTRONIC_TRANSFER("Electronic Transfer"),
   GIRO("Giro"),
   GRAY_PAY("Grab Pay"),
