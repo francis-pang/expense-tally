a8a021bbe5624636cc1d5bd4a02de097b0cecbcb
boyshawn <boyshawn@hotmail.com>
2021-01-17 14:02:13 +0800

    Change to use EnumTypeHandler for enum type parameter

--- expense-manager/src/main/resources/local-development/expense_manager.sql
+++ expense-manager/src/main/resources/local-development/expense_manager.sql
@@ -2,13 +2,13 @@
 
 CREATE TABLE expense_manager
 (
-    id               INTEGER PRIMARY KEY, -- row identifier
-    amount           DECIMAL(8, 3),       -- expensed amount
-    category         VARCHAR(255),        -- category TODO: To put into enum
-    subcategory      VARCHAR(255),        -- L2 category TODO: To put into enum
-    payment_method   VARCHAR(255),        -- Payment method TODO: To put into enum
-    description      VARCHAR(512),
-    expensed_time    DATETIME,            -- Transaction time
-    reference_amount DECIMAL(8, 3)        -- Amount shown in the bank statement or receipt, without considering the
-    -- split cost)
+    id               INTEGER PRIMARY KEY,    -- row identifier
+    amount           DECIMAL(8, 3) NOT NULL, -- expensed amount
+    category         VARCHAR(255) NOT NULL,  -- category TODO: To put into enum
+    subcategory      VARCHAR(255) NOT NULL,  -- L2 category TODO: To put into enum
+    payment_method   VARCHAR(255) NOT NULL,  -- Payment method TODO: To put into enum
+    description      VARCHAR(512) NOT NULL,
+    expensed_time    DATETIME NOT NULL,      -- Transaction time
+    reference_amount DECIMAL(8, 3)           -- Amount shown in the bank statement or receipt, without considering the
+                                             -- split cost
 )
